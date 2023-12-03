4dbfc9b813897abbeefe3628900d10eca581ef93
Alexey Kuznetsov <me@kuznetsoff.io>
2023-12-03 15:36:21 +0100

    initial commit

---
+++ rust/daily-template/benches/benchmarks.rs
@@ -0,0 +1,16 @@
+use {{crate_name}}::*;
+
+fn main() {
+    // Run registered benchmarks.
+    divan::main();
+}
+
+#[divan::bench]
+fn part1() {
+    part1::process(divan::black_box(include_str!("../input/input1.txt",))).unwrap();
+}
+
+#[divan::bench]
+fn part2() {
+    part2::process(divan::black_box(include_str!("../input/input2.txt",))).unwrap();
+}
