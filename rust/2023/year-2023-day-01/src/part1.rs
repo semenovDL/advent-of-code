ecee61f43d7768530c7ff152598aeba683fb66dd
Alexey Kuznetsov <me@kuznetsoff.io>
2023-12-05 00:57:32 +0100

    day 1

--- rust/2023/year-2023-day-01/src/part1.rs
+++ rust/2023/year-2023-day-01/src/part1.rs
@@ -1,8 +1,21 @@
 use crate::custom_error::AocError;
 
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let res = input
+        .lines()
+        .map(|line| {
+            let first = line.chars().find_map(|c| c.to_digit(10));
+            let last = line.chars().rev().find_map(|c| c.to_digit(10));
+
+            match (first, last) {
+                (Some(a), Some(b)) => a * 10 + b,
+                _ => 0,
+            }
+        })
+        .sum::<u32>()
+        .to_string();
+    Ok(res)
 }
 
 #[cfg(test)]
@@ -11,9 +24,11 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        assert_eq!("142", process(input)?);
         Ok(())
     }
 }
--- rust/2023/year-2023-day-01/src/part2.rs
+++ rust/2023/year-2023-day-01/src/part2.rs
@@ -1,8 +1,35 @@
 use crate::custom_error::AocError;
 
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let res = input
+        .lines()
+        .map(|line| {
+            let first = (0..line.len()).find_map(|i| find_in_slice(&line[i..]));
+            let last = (0..line.len())
+                .rev()
+                .find_map(|i| find_in_slice(&line[i..]));
+
+            match (first, last) {
+                (Some(a), Some(b)) => a * 10 + b,
+                _ => 0,
+            }
+        })
+        .sum::<u32>()
+        .to_string();
+    Ok(res)
+}
+
+const STR_NUMBERS: [&str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+
+fn find_in_slice(slice: &str) -> Option<u32> {
+    STR_NUMBERS
+        .iter()
+        .position(|str_digit| slice.starts_with(str_digit))
+        .map(|i| i as u32 + 1)
+        .or_else(|| slice.chars().next().and_then(|c| c.to_digit(10)))
 }
 
 #[cfg(test)]
@@ -11,9 +38,14 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+        assert_eq!("281", process(input)?);
         Ok(())
     }
 }
